665b3656cae4272d6dd8e1edfb8d421083038691
unknown <bguzman@bguzmanpw7.indra.es>
2014-10-29 08:41:04 +0100

    Errors controlled on backspace and delete. Each time user advances in writting pattern, if a token is found, the position of the caret will advance by 1 position

--- js/mask.js
+++ js/mask.js
@@ -17,6 +17,7 @@ var Mask = {};
     
     
     function Masker($el, mask, errorFunction){
+        this.linkedMask = [];
         this.$el = $el;
         this.mask = mask.trim();
         this.errorFunction = errorFunction;
@@ -24,35 +25,61 @@ var Mask = {};
         this.setMask();
     };
     
+    Masker.prototype.linkedMask = [];
     Masker.prototype.specialModifiers = {};
     Masker.prototype.errors = {};
     Masker.prototype.doNotValidate = {};
     Masker.prototype.BACKSPACE = 8;
     Masker.prototype.DELETE = 46;
+    Masker.prototype.tokensBefore = [];
     
     Masker.prototype.addExtraChars = function(){
         var i = 0;
         var nCharsInMask = this.mask.length;
         var char;
         var functionAssociated;
+        var context = this;
+        var prevChar;
+        var nextChar;
         var existsCompleteTemplate = this.templates[this.mask];
         for(; i<nCharsInMask && !existsCompleteTemplate ; i++){
             char = this.mask[i];
             functionAssociated = this.templates[char];
             if(!functionAssociated){
-                this.templates[char] = function(extraChar, input, context){
-                    var pos = context.$el.get(0).selectionStart;
-                    var charAtMask = context.mask[pos];
-                    var nextChar = input.charAt(pos);
-                    //context.lastInput = charAtMask;
-                    return extraChar == charAtMask && extraChar != nextChar;
-                }
-                this.validator[char] = function(){
-                    return true;
-                }
-                this.doNotValidate[char] = true;
+                createTemplateFunction();
+                createValidationFunction();
+                createException();
             }
+            var charObject = {
+                pos: i,
+                maskValue: char
+            }
+            if(i>0){
+                prevChar = this.linkedMask[i-1];
+                charObject.prevChar = prevChar;
+                prevChar.nextChar = charObject;
+            }
+            this.linkedMask.push(charObject);
         }
+        
+        function createTemplateFunction(){
+            context.templates[char] = function(extraChar, input, context){
+                var pos = context.$el.get(0).selectionStart;
+                var charAtMask = context.mask[pos];
+                var nextChar = input.charAt(pos);
+                return extraChar == charAtMask && extraChar != nextChar;
+            }
+        };
+        
+        function createValidationFunction(){
+            context.validator[char] = function(){
+                return true;
+            }
+        };
+        
+        function createException(){
+            context.doNotValidate[char] = true;
+        };
     };
     
     Masker.prototype.setMask = function(){
@@ -62,12 +89,37 @@ var Mask = {};
             this.specialMask = this.isSpecialMask(this.mask, this);
             
             this.$el.on("keydown", function(e){
-                var input = $(this).val();
-                var pos = $(this).get(0).selectionStart - 1;
+                var input = $(this)
+                var inputVal = input.val();
+                var pos = $(this).get(0).selectionStart;
+                var backPosition = pos-1;
                 var char;
-                if(e.keyCode == context.BACKSPACE){
-                    char = input.charAt(pos);
+                var mustCheck = false;
+                
+                if(isBackSpace()){
+                    pos--;
+                    mustCheck = true;
+                }else if(isDelete()){
+                    mustCheck = true;
+                }
+                
+                if(mustCheck){
+                    mustPrevent();
+                }
+                
+                function isBackSpace(){
+                    return e.keyCode == context.BACKSPACE;
+                };
+                
+                function isDelete(){
+                    return e.keyCode == context.DELETE;
+                };
+                
+                function mustPrevent(){
+                    char = inputVal.charAt(pos);
                     if(context.doNotValidate[char]){
+                        input.get(0).selectionStart = backPosition;
+                        input.get(0).selectionEnd = backPosition;
                         e.preventDefault();
                     }
                 }
@@ -79,12 +131,17 @@ var Mask = {};
                 var newChar = String.fromCharCode(e.keyCode);
                 var inputParent = $(this).parent();
                 var actualPos = $(this).get(0).selectionStart;
-                context.event = e;
                 
+                context.event = e;
                 composeFinalInput();
-                context.completePattern(e, context);
+                context.completePattern();
                 if(context.validPattern){
                     validateValue();
+                }
+                if(context.advancePosition){
+                    context.advancePosition = false;
+                    context.$el.get(0).selectionStart = actualPos + 1 ;
+                    context.$el.get(0).selectionEnd = actualPos + 1 ;
                 }
                 
                 function composeFinalInput(){
@@ -98,6 +155,10 @@ var Mask = {};
                     if(validFormat == false){
                         context.errors[context.lastInput] = true;    
                     }else{
+                        if(context.linkedMask[actualPos]){
+                            context.linkedMask[actualPos].inputVal = newChar;
+                        }
+                        
                         if(typeof context.errors[context.lastInput]){
                             context.errors[context.lastInput] = false;
                         }
@@ -118,51 +179,6 @@ var Mask = {};
         }
     };
     
-    Masker.prototype.insertCharInCaret = function(newChar, inputVal, actualPos){
-        var firstPart = inputVal.substring(0 , actualPos);
-        var endPart = inputVal.substring(actualPos);
-        return firstPart + newChar + endPart;
-    };
-    
-    Masker.prototype.autoCompleteWithToken = function(context){
-        var pos = context.$el.get(0).selectionStart + 1;
-        var maskChar = context.mask[pos];
-        var inputChar = context.finalValue[pos];
-        if(context.doNotValidate[maskChar] && maskChar != inputChar){
-            var finalInput = context.insertCharInCaret(maskChar, context.finalValue, pos);
-            context.$el.val(finalInput);
-            context.event.preventDefault();
-        }
-    };
-    
-    Masker.prototype.validateValue = function(context, lastChar){
-        var validationFunction;
-        var charToValidate = lastChar || context.lastInput;
-        var isValid = context.doNotValidate[charToValidate];
-        var valueForLastChar = context.getValueFor(charToValidate, context.finalValue, context);
-        if(!isValid && valueForLastChar.patternCount == valueForLastChar.inputCount){
-            validationFunction = context.validator[charToValidate];
-            isValid = validationFunction(valueForLastChar.value, context, true);
-            if(isValid){
-                context.autoCompleteWithToken(context);
-                //var finalVal = context.insertCharInCaret(newChar, inputVal, actualPos);
-                
-            }
-        }
-        return isValid;
-    };
-    
-    
-    Masker.prototype.checkOldErrors = function(context){
-        var stillValid ;
-        $.each(context.errors, function(i, val){
-            if(val == true && stillValid!= false){
-                stillValid = context.validateValue(context, i);
-            }
-        });
-        return stillValid;
-    }
-        
     Masker.prototype.isSpecialMask = function(mask, context){
         var mask = context.mask;
         var maskLength = mask.length;
@@ -183,40 +199,54 @@ var Mask = {};
         }
         
         return pattern;        
-    }
+    };
     
-    Masker.prototype.completePattern = function(e, context){
-        var value = context.value = $(e.target).val();
+    Masker.prototype.insertCharInCaret = function(newChar, inputVal, actualPos){
+        var firstPart = inputVal.substring(0 , actualPos);
+        var endPart = inputVal.substring(actualPos);
+        return firstPart + newChar + endPart;
+    };
+    
+    Masker.prototype.completePattern = function(){
+        var event = this.event;
+        var value = this.value = $(event.target).val();
+        var context = this;
         var length = value.length;
-        var pattern = context.mask;
-        var lastChar = String.fromCharCode(e.keyCode);
-        var charNow = pattern[context.$el.get(0).selectionStart];
-        var specialMask = context.specialMask;
+        var pattern = this.mask;
+        var lastChar = String.fromCharCode(event.keyCode);
+        var pos = this.$el.get(0).selectionStart;
+        var charNow = pattern[pos];
+        var specialMask = this.specialMask;
         var mustPrevent = false;
-        var event = e;
         var functionToApply;
+        this.getTokensBefore(pos);
+        var groupComparison;
+        var newPos;
         
-        context.lastInput = charNow;
         if(isCharInPattern()){
+            getNewCharNow();
             getTemplateChecker();
-            if(functionToApply){
-                checkTemplate();
-            }
-            
-            if(context.validPattern == false){
-                setPrevent();
-            }else{
-                context.written[charNow] = true;
-            }
+            checkTemplate();
+            mustAdvancePosition();
+            isValidPattern();            
         }else{
             setPrevent();
         }
         preventWriting();
         
         
-        
         function isCharInPattern(){
             return typeof charNow != "undefined" || specialMask;
+        };
+        
+        function getNewCharNow(){
+            groupComparison = context.getGroupValue(pos);
+            newPos = context.mask.indexOf(groupComparison.group) + groupComparison.groupCount;
+            if(groupComparison.value.length > groupComparison.groupCount){
+                charNow = context.lastInput = context.mask[newPos];
+            }else{
+                charNow = context.lastInput = groupComparison.group;
+            }
         };
         
         function getTemplateChecker(){
@@ -224,7 +254,23 @@ var Mask = {};
         };
         
         function checkTemplate(){
-            context.validPattern = functionToApply(lastChar, value, context);
+            if( functionToApply){
+                context.validPattern = functionToApply(lastChar, value, context);
+            }else{
+                context.validPattern = false;
+            }
+        };
+        
+        function mustAdvancePosition(){
+            if(context.doNotValidate[charNow]){
+                context.advancePosition = true;
+            }
+        };
+        
+        function isValidPattern(){
+            if(context.validPattern == false){
+                setPrevent();
+            }
         };
         
         function setPrevent(){
@@ -236,7 +282,162 @@ var Mask = {};
                 event.preventDefault();
             }
         };
+    };
+    
+    Masker.prototype.validateValue = function(context, lastChar){
+        var validationFunction;
+        var charToValidate = lastChar || context.lastInput;
+        var isValid = context.doNotValidate[charToValidate];
+        var valueForLastChar = context.getValueFor(charToValidate, context.finalValue, context);
+        if(!isValid && valueForLastChar.patternCount == valueForLastChar.inputCount){
+            validationFunction = context.validator[charToValidate];
+            isValid = validationFunction(valueForLastChar.value, context, true);
+            if(isValid){
+                context.autoCompleteWithToken(context);
+            }
+        }
+        return isValid;
+    };
+    
+    Masker.prototype.autoCompleteWithToken = function(context){
+        var pos = context.$el.get(0).selectionStart + 1;
+        var maskChar = context.mask[pos];
+        var inputChar = context.finalValue[pos];
         
+        if(context.doNotValidate[maskChar] && maskChar != inputChar){
+            var finalInput = context.insertCharInCaret(maskChar, context.finalValue, pos);
+            context.$el.val(finalInput);
+            context.event.preventDefault();
+        }
+       
+    };
+    
+    Masker.prototype.checkOldErrors = function(context){
+        var stillValid ;
+        $.each(context.errors, function(i, val){
+            if(val == true && stillValid!= false){
+                stillValid = context.validateValue(context, i);
+            }
+        });
+        return stillValid;
+    }
+    
+    Masker.prototype.getGroupValue = function(pos){
+        this.getTokensBefore(pos);
+        var group = this.getPositionInMask();
+        var groupCount = this.getNumberOf(group);
+        var bounds = this.getInputGroupBounds();
+        if(bounds.start == -1){
+            bounds.start = 0;
+        }
+        
+        if(bounds.end == -1){
+            bounds.end = pos + groupCount;
+        }
+        var value = this.finalValue.substring(bounds.start, bounds.end);
+        return {
+            value: value,
+            group: group,
+            groupCount: groupCount,
+            isCompleted: groupCount == value.length
+        }
+    };
+    
+    Masker.prototype.getInputGroupBounds = function(){
+        var input = this.finalValue;
+        var i = 0;
+        var inputLength = input.length;
+        var char;
+        var tokenCount = 0;
+        var firstPosition = -1;
+        var lastPosition = -1;
+        var boundaries = {
+            start: -1,
+            end: -1
+        };
+        for(; i<inputLength ; i++){
+            char = input[i];
+            if(this.doNotValidate[char]){
+                if(char == this.tokensBefore[tokenCount]){
+                    firstPosition = i+1;
+                    tokenCount++;
+                }
+            }
+        }
+        if(firstPosition < 0){
+            firstPosition = 0;
+        }
+        boundaries.start = firstPosition;
+        for(; firstPosition < inputLength; firstPosition++){
+            char = input[firstPosition];
+            if(this.doNotValidate[char]){
+                lastPosition = firstPosition;
+                firstPosition = inputLength;
+            }
+        }
+        if(lastPosition < 0){
+            lastPosition = inputLength;
+        }
+        boundaries.end = lastPosition;
+        
+        return boundaries;
+    };
+    
+    Masker.prototype.getNumberOf = function(char){
+        var pattern = this.mask;
+        var i = 0;
+        var patternLegth = pattern.length;
+        var count = 0;
+        for(; i<patternLegth ; i++){
+            if(char == pattern[i]){
+                count++;
+            }
+        }
+        return count;
+    };
+    
+    Masker.prototype.getTokensBefore = function(pos){
+        this.tokensBefore = [];
+        var i = 0;
+        var char;
+        this.distanceFromToken = 0;
+        for(; i < pos ; i++){
+            char = this.finalValue[i];
+            this.distanceFromToken++;
+            if(this.doNotValidate[char]){
+                this.distanceFromToken = 1;
+                this.tokensBefore.push(char);
+            }
+        }
+    };
+    
+    Masker.prototype.getPositionInMask = function(){
+        var token;
+        var tokensBefore = this.tokensBefore;
+        var nTokens = tokensBefore.length;
+        var i = 0;
+        var j;
+        var lastPositionInMask = 0;
+        var mask = this.mask;
+        var maskLength = mask.length;
+        var maskChar;
+        var group = "";
+        for(; i< nTokens ; i++){
+            j = lastPositionInMask;
+            token = tokensBefore[i];
+            for(; j < maskLength ; j++){
+                maskChar = mask[j];
+                if( this.doNotValidate[maskChar] && token == maskChar){
+                    group = mask[j+1];
+                    lastPositionInMask = j+1;
+                    j = maskLength;
+                }
+            }
+        }
+        if(i == 0){
+            group = mask[0];
+        }
+        return group;
     };
         
     Masker.prototype.getValueFor = function(patternChar, input, context){
